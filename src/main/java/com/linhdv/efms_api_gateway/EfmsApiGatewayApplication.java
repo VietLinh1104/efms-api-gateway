a47c99b0e2d2b12c47e3a7d70b26c4d2788d1beb
OS.NGS-MINHDQ <os.ngs-minhdq@vietinbank.vn>
2026-03-28 19:00:25 +0700

    init

---
+++ src/main/java/com/linhdv/efms_api_gateway/EfmsApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.linhdv.efms_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfmsApiGatewayApplication {
+
+	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+		SpringApplication.run(EfmsApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linhdv/efms_api_gateway/EfmsApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linhdv.efms_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfmsApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
